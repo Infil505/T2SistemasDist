1c84adf940d19558a76438122d83c1031d70c70e
infil5052 <darwin.silva.gadea@est.una.ac.cr>
2025-08-24 21:56:14 -0600

    refactor: clean up root controller by removing commented code

--- controllers/root.js
+++ controllers/root.js
@@ -1,8 +1,4 @@
 'use strict';
 module.exports.home = (req, res) => {
-    // O devuelve JSON…
-    // res.json({ message: 'Library API up' });
-
-    // …o redirige al Swagger UI
     res.redirect('/docs');
 };
